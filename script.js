7176dbba495403b1ae54d4c4f205b1ab220ac82a
MessiasR <messiasamaral123@gmail.com>
2023-12-12 16:11:35 -0300

    Refactoring

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+const btnTry = document.querySelector("#btnTry")
+const btnAgain = document.querySelector("#btnAgain")
 const screen1 = document.querySelector(".screen1")
 const screen2 = document.querySelector(".screen2")
+
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
+
+btnTry.addEventListener('click', handleTryCLick)
+btnAgain.addEventListener('click', handleAgainClick)
+document.addEventListener('keydown', enterKey)
 
 function handleTryCLick(event) {
     event.preventDefault()
@@ -9,8 +16,7 @@ function handleTryCLick(event) {
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
-        screen1.classList.add("hide")
-        screen2.classList.remove("hide")
+        toggleScreen()
         
         if (xAttempts == 1) {
             document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativa!`
@@ -26,16 +32,19 @@ function handleTryCLick(event) {
 }
 
 function handleAgainClick() {
-    screen1.classList.remove("hide")
-    screen2.classList.add("hide")
-
+    toggleScreen()
     xAttempts = 1
 
     randomNumber = Math.round(Math.random() * 10)
 }
 
-const btnTry = document.querySelector("#btnTry")
-const btnAgain = document.querySelector("#btnAgain")
+function toggleScreen() {
+    screen1.classList.toggle("hide")
+    screen2.classList.toggle("hide")
+}
 
-btnTry.addEventListener('click', handleTryCLick)
-btnAgain.addEventListener('click', handleAgainClick)
+function enterKey(e) {
+    if(e.key == 'Enter' && screen1.classList.contains('hide')) {
+        handleAgainClick()
+    }
+}
