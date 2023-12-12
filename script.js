ff60bcf8e91f44351c2bc9bc06dc5358914c882f
MessiasR <messiasamaral123@gmail.com>
2023-12-12 15:02:40 -0300

    Project finish

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const randomNumber = Math.round(Math.random() * 10)
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
 
-console.log(randomNumber)
-
-function handleCLick(event) {
+function handleTryCLick(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if(Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide")
-        document.querySelector(".screen2").classList.remove("hide")
+        screen1.classList.add("hide")
+        screen2.classList.remove("hide")
         
         if (xAttempts == 1) {
             document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} tentativa!`
@@ -20,10 +20,22 @@ function handleCLick(event) {
         }
     }
 
+    inputNumber.value = ""
+
     xAttempts++
 }
 
-function again() {
-    document.querySelector(".screen1").classList.remove("hide")
-    document.querySelector(".screen2").classList.add("hide")
+function handleAgainClick() {
+    screen1.classList.remove("hide")
+    screen2.classList.add("hide")
+
+    xAttempts = 1
+
+    randomNumber = Math.round(Math.random() * 10)
 }
+
+const btnTry = document.querySelector("#btnTry")
+const btnAgain = document.querySelector("#btnAgain")
+
+btnTry.addEventListener('click', handleTryCLick)
+btnAgain.addEventListener('click', handleAgainClick)
